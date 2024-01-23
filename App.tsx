94f222a808d12ebdf58d3f1aa4e8d8ff33cb0f07
Amós Silas <silastj@hotmail.com>
2024-01-22 23:09:59 -0300

    add more code studyng

--- App.tsx
+++ App.tsx
@@ -3,10 +3,22 @@ import { StyleSheet, Button, Text, SafeAreaView, Alert, View } from 'react-nativ
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Item from './components/Item';
+import { Person } from './types/Person'
+
 
 export default function App() {
   const [count, setCount] = useState(0);
   const [nome, setNome] = useState<string>('')
+  const [person, setPerson] = useState<Person>({
+    name: "Silas",
+    age: 10,
+    height: 120
+  })
+  const [ingredientes, setIngredientes] = useState<string[]>([
+    'farinha',
+    'ovo',
+    'manteiga'
+  ])
 
   const handleNomeInputChange = (text: string) => {
     setNome(text)
@@ -16,10 +28,45 @@ export default function App() {
     Alert.prompt('Informe  o seu nome', 'Digite o seu nome', handleNomeInputChange)
   }
 
+  // const handleClick = () => {
+  //   Alert.alert('Apertou button')
+  // }
+
+  const changeName = (newName: string) => {
+    setPerson({ ...person, name: newName })
+  }
+
+  const changeAge = (newAge: number) => {
+    setPerson({ ...person, age: newAge })
+  }
+
+  const handleMoreItem = () => {
+    setIngredientes([...ingredientes, 'NOVO NOVO'])
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Header name={nome} />
       <Text style={styles.text}>You clicked {count} times</Text>
+
+      <Text>Aqui é o NOME: {person.name}</Text>
+      <Text>Aqui é o IDADE: {person.age}</Text>
+
+      <Button
+        color="#ff0000"
+        onPress={() => changeName('novoNome')}
+        title="Troque o NOME"
+      />
+      <Button
+        color="#ff0000"
+        onPress={() => changeName('SAMRA')}
+        title="Troque o Novo NOME"
+      />
+      <Button
+        color="#ff0000"
+        onPress={() => changeAge(100)}
+        title="Troque o AGE"
+      />
       <Button
         color="#fff"
         onPress={() => setCount(count + 1)}
@@ -35,23 +82,33 @@ export default function App() {
         title="Digite o seu nome"
         onPress={exibirAlerta}
       />
-      <View sytle={styles.body}>
+      <View style={styles.body}>
         <Item
+          onPress={() => Alert.alert('Clicou em QUeijo')}
           title="Quatro Queijos"
           price={50}
           ingredientes={["queijo", "massa", "oregano"]}
         />
         <Item
+          onPress={() => Alert.alert('Clicou em Atum')}
           title="Atum"
           price={70}
           ingredientes={["atum", "massa", "oregano", "maionese"]}
         />
         <Item
+          onPress={() => Alert.alert('Clicou em Bufala')}
           title="Mussarela de Bufala"
           price={90}
           ingredientes={["queijo", "massa", "oregano", "tomate seco", "mussarela", "azeite"]}
         />
+        {ingredientes.map((item, index) => (
+          <Text key={index}>{item}</Text>
+        ))}
       </View>
+      <Button 
+        onPress={handleMoreItem}
+        title="adicionar Item"
+      />
       <Footer />
     </SafeAreaView>
   );
@@ -79,7 +136,7 @@ const styles = StyleSheet.create({
     bottom: 50,
     color: 'white'
   },
-  body:{
+  body: {
     flex: 1
   }
 });
--- components/Item/index.tsx
+++ components/Item/index.tsx
@@ -1,17 +1,15 @@
-import {StyleSheet, Alert, TouchableHighlight, Text, View } from "react-native"
+import {StyleSheet, TouchableHighlight, Text, View } from "react-native"
 
 type Props = {
   title: string
   price: number
   ingredientes: string[]
+  onPress: () => void
 }
 
-const Item = ({title, price, ingredientes}: Props) => {
-  const handleClick = () => {
-    Alert.alert('Apertou button')
-  }
+const Item = ({title, price, ingredientes, onPress}: Props) => {
   return (
-    <TouchableHighlight onPress={handleClick} style={styles.container}>
+    <TouchableHighlight onPress={onPress} style={styles.container}>
       <View>
         <Text><Text style={styles.bold}>Pizza: </Text> {title}</Text>
         <Text><Text style={styles.bold}>Pizza: </Text>{price.toFixed(2)}</Text>
